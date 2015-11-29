bf23a519f41e57966a603860a4e95b886f91a11a
klapnebojsa <klapnebojsa@gmail.com>
2015-11-29 02:24:23 +0100

    .

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure0402
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/clojure0402/core.clj
@@ -0,0 +1,7 @@
+(ns clojure0402.core)
+
+(refer 'clojure0401.core)
+
+@#'clojure0401.core/everything   ;poziva promenjivu Var iz clojure0401.core
+;42
+
